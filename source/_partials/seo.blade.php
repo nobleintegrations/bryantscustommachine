dae275dd95a4a187ea7d0e652bc8b3b90e7b90fb
Tanner Bryant <tanner@nobleintegrationsllc.com>
2018-09-15 10:15:48 -0500

    added a few seo tags

---
+++ source/_partials/seo.blade.php
@@ -0,0 +1,7 @@
+        <title>West Texas Premier Machine Shop | Bryants Custom Machine</title>
+        <meta name="description" content="Description here no more than 300 char">
+        <meta property="og:url" content="https://bryantscustommachine.netlify.com" />
+        <meta property="og:title" content="West Texas Premier Machine Shop | Bryants Custom Machine" />
+        <meta property="og:description" content=""/>
+        <meta property="og:image" content="" />
+        {{-- <meta name="robots" content="index, follow"> --}}
