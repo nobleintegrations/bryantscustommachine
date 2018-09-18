921f8598a18f616f0b7af05f6a0f61b8476a6a89
Tanner Bryant <tanner@nobleintegrationsllc.com>
2018-09-18 13:27:53 -0500

    Added purgeCSS to remove unusued code

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,18 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 var tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+}
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -18,3 +30,31 @@ mix.js('source/_assets/js/main.js', 'js')
         processCssUrls: false,
         postCss: [tailwindcss('tailwind.js')],
     }).version();
+
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, "source/**/*.blade.php"),
+                    //path.join(__dirname, "resources/assets/js/**/*.vue")
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ["html", "js", "php", "vue"]
+                    }
+                ]
+            })
+        ]
+    });
+}
