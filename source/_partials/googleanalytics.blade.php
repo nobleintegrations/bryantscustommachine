9aed2fdcfde8a83c0ed267b035a3ebe9d0856465
Tanner Bryant <tanner@nobleintegrationsllc.com>
2020-03-11 12:40:39 -0500

    Update googleanaltyics and seo partials to reflect new domain

--- source/_partials/googleanalytics.blade.php
+++ source/_partials/googleanalytics.blade.php
@@ -1,1 +1,10 @@
     {{-- <meta>google analytics here</meta> --}}
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74522377-16"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-74522377-16');
+</script>
