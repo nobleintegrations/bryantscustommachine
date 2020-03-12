2a8ad25baecbd8ba034b3a2ddd18bd698eda5564
Tanner Bryant <tanner@nobleintegrationsllc.com>
2020-03-12 08:25:10 -0500

    Fixed seo issue with opengraph tag

--- source/_partials/seo.blade.php
+++ source/_partials/seo.blade.php
@@ -1,7 +1,7 @@
         <title>West Texas Premier Machine Shop | Bryants Custom Machine</title>
         <meta name="description" content="As a family owned and operated business, we strive to put our customers’ needs first. We stand behind our product 100% and guarantee each part received will be of highest quality and within the industries specifications.">
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://bryantscustommachine.com" />
+        <meta property="og:url" content="https://www.bryantscustommachine.com" />
         <meta property="og:title" content="West Texas Premier Machine Shop | Bryants Custom Machine" />
         <meta property="og:description" content="As a family owned and operated business, we strive to put our customers’ needs first. We stand behind our product 100% and guarantee each part received will be of highest quality and within the industries specifications."/>
         <meta property="og:image" content="https://res.cloudinary.com/noble-integrations/image/upload/v1583946473/BCM/twitter_card2_tfygfe.png" />
